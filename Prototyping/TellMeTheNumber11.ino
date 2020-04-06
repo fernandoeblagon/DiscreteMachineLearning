6419cfc956411d5b5f88b8a9e59d7b0d4013e7ba
fernandoeblagon <58393282+fernandoeblagon@users.noreply.github.com>
2020-04-06 01:18:06 +0100

    Add files via upload

---
+++ Prototyping/TellMeTheNumber11.ino
@@ -0,0 +1,32 @@
+/*
+  ReadAnalogVoltage
+
+  Reads an analog input on pin 0, converts it to voltage, and prints the result to the Serial Monitor.
+  Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
+  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/ReadAnalogVoltage
+*/
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+//  int sensorValue = analogRead(A0);
+    int sensorValue1 = analogRead(A1);
+//  int sensorValue2 = analogRead(A2);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  float voltage = sensorValue1 * (5 / 1023.0);
+  float voltage1 = sensorValue1  * (5 / 1023.0);
+  float voltage2 = sensorValue1  * (5 / 1023.0);
+      Serial.println(String(voltage1) +" " + String(voltage1) + " " + String(voltage1));
+  // print out the value you read:
+  // Serial.println(String(voltage) +" " + String(voltage1) + " " + String(voltage2));
+}
